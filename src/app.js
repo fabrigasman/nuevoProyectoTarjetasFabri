c251cfe4768ded3e985aa550c42839e8abf3d6c1
Fabrigasman <fabriciogasaro@gmail.com>
2025-03-03 17:30:37 +0000

    proyectoDeCartasFabri

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  
+  document.getElementById("generateButton").addEventListener("click", generateCard);
 };
+
+function generateCard() {
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuitNumber = Math.floor(Math.random() * suit.length);
+  let finalSuit = suit[randomSuitNumber];
+  
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "";
+  document.getElementById('theCard').classList.add("card");
+  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+}
+
+function getSuiteClass(suit) {
+  switch (suit) {
+    case "Diamonds": return "suit-diamonds";
+    case "Spades": return "suit-spades";
+    case "Hearts": return "suit-hearts";
+    case "Clubs": return "suit-clubs";
+  }
+}
